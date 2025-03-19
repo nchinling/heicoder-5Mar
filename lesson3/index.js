965d85e0a86ae5ba63639b2ef8fbaf2215f41132
Ng Chin Ling <nchinling@gmail.com>
2025-03-19 22:04:13 +0800

    lesson 3 notes

---
+++ lesson3/index.js
@@ -0,0 +1,16 @@
+//Exercise 1
+let stockPrice = 300;
+
+stockPrice = 
+console.log(stockPrice)
+
+//Exercise 2
+let newStockPrice = 10;
+
+newStockPrice = newStockPrice * 3;
+newStockPrice *= 3;
+console.log(newStockPrice)
+
+//Exercise 3
+let myArray = [2,3,5];
+console.log(myArray[2]);
---
+++ lesson3/notes.js
@@ -0,0 +1,38 @@
+
+// String literals
+const formattedRating = `My Rating for this Drink is ${rating}/${maxScore}`;
+
+// string --> int
+age = parseInt(age);
+
+// Int --> String
+age = age.toString();
+
+
+//array operations
+array.push(90); 
+array.shift(40);
+array.push(50);
+array.unshift(200);
+
+array.slice(1,4)
+select index 1 to 3 (4 - 1)
+
+TSLAClosingPrices.pop(); // remove the LAST element from the back.
+TSLAClosingPrices.shift(); // remove the first element from the front
+TSLAClosingPrices.push(100.0); // adds an element TO THE BACK
+TSLAClosingPrices.unshift(900); // adds an element to the FRONT
+
+TSLAClosingPrices = TSLAClosingPrices.slice(1, 4);
+TSLAClosingPrices = TSLAClosingPrices.sort();
+TSLAClosingPrices = TSLAClosingPrices.reverse();
+
+//Objects
+const inventoryItemCookie = {
+    itemName: "cookie",
+    qty: 100,
+    price: "SG$1.10",
+  };
+  
+  const formattedMessage = `The price of a ${inventoryItemCookie.itemName} is ${inventoryItemCookie.price}`;
+  console.log(formattedMessage);
