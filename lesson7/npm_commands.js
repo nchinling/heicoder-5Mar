fcdfb1347560415f5663d9719ba385cce66c28f1
Ng Chin Ling <nchinling@gmail.com>
2025-04-18 15:45:53 +0800

    updates

--- lesson7/npm_commands.js
+++ lesson7/npm_commands.js
@@ -3,3 +3,10 @@
 3. npm create vite@latest my-react-app -- --template react
 
 
+npm create vite@latest NgChinLing-Capstone -- --template react
+
+
+cd ngchinling-capstone
+npm install
+npm run dev
+
